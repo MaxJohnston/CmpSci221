c725cc8f3ff43d3d9769f3b381436f495c747a17
MaxJohnston <maxjohnston1997@gmail.com>
2016-11-28 11:34:56 -0500

    Update GameFrame.java

--- GameFrame.java
+++ GameFrame.java
@@ -1,4 +1,4 @@
-// Game Demo
+//Jack Roboski and Max Johnston
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,6 +24,7 @@ public class GameFrame extends JPanel implements ActionListener {
     int level = 1;
     
     static ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+    static ArrayList<PowerUp> powerups = new ArrayList<PowerUp>();
     static ArrayList<Missile> missiles = new ArrayList<Missile>();
     Random rand = new Random();
     
@@ -43,7 +44,7 @@ public class GameFrame extends JPanel implements ActionListener {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         
-        ImageIcon ic = new ImageIcon("C:\\Users\\Gomensnana\\Desktop\\DemoMissile.png");
+        ImageIcon ic = new ImageIcon("DemoMissile.png");
         g2d.drawImage(ic.getImage(), 0, 0,null);
         
         player.draw(g2d);
@@ -57,6 +58,11 @@ public class GameFrame extends JPanel implements ActionListener {
             Missile m = missiles.get(i);
             m.draw(g2d);
         }
+        
+        for (int i = 0; i < powerups.size(); i++){
+            PowerUp tempPU = powerups.get(i);
+            tempPU.draw(g2d);
+        }
     }
     
     @Override
@@ -66,6 +72,11 @@ public class GameFrame extends JPanel implements ActionListener {
         for (int i = 0; i < enemies.size(); i++){
             Enemy tempEnemy = enemies.get(i);
             tempEnemy.update();
+        }
+        
+        for (int i = 0; i < powerups.size(); i++){
+            PowerUp tempPU = powerups.get(i);
+            tempPU.update();
         }
         
         for (int i = 0; i < missiles.size();i++){
@@ -102,20 +113,32 @@ public class GameFrame extends JPanel implements ActionListener {
         return missiles;
     }
     
+    public void addPowerUp(PowerUp u) {
+        powerups.add(u);
+    }
+    
+    public static void removePowerUp(PowerUp u){
+        powerups.remove(u);
+    }
+    
+    public static ArrayList<PowerUp> getPowerUpList(){
+        return powerups;
+    }
     public void startGame(){
         enemyCount = level*5;
         for (int i = 0; i < enemyCount; i++){
             addEnemy(new Enemy(rand.nextInt(500),-rand.nextInt(600)));
         }
+        addPowerUp(new PowerUp(rand.nextInt(500),-rand.nextInt(600)));
     }
     public void checkEnd(){
-        if(enemies.size() == 0){
+        if(enemies.size() == 0 &&(powerups.size()==0)){
             level++;
             enemies.clear();
             missiles.clear();
+            powerups.clear();
             JOptionPane.showMessageDialog(this, "You've won this round!");
             startGame();
         }
     }
 }
-
